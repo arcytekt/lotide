c0e0624d1800ffcff59e7be4eb22cb380a747678
Ethan Pelensky <ethan@arcytekt.com>
2024-01-08 21:44:11 -0700

    Finalized version of tail function based off of assertEqual function

--- tail.js
+++ tail.js
@@ -14,3 +14,6 @@ const tail = function(array) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words).length, words.length - 1) // tail should have one element less than its input
+assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
