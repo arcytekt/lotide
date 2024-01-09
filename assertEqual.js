6a2e9c026697f995e610dabe7007ccace6d78a86
Ethan Pelensky <ethan@arcytekt.com>
2024-01-08 20:41:18 -0700

    Finalized version of assertion function w/ template literals.

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   const pass = actual === expected;
   if (pass) {
-    console.log("✅✅✅ Assertion Passed: [" + actual + "] === [" + expected + "]");
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 
