dc26587dc8b98b4507bac470321d6536a3ccffe3
Ethan Pelensky <ethan@arcytekt.com>
2024-01-08 20:55:43 -0700

    Finalized version of head function based off of assertEqual function

--- head.js
+++ head.js
@@ -12,7 +12,6 @@ const head = function(array) {
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(head(["Lighthouse", "Labs"]), "Lighthouse");
 assertEqual(head([1, 2, 3]), 1);
 assertEqual(head(["test", "case"]), "test");
