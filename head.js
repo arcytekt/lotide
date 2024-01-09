67dcfb050f332fd3d3fd2462c163706b045e068b
Ethan Pelensky <ethan@arcytekt.com>
2024-01-08 20:59:21 -0700

    Finalized version of head function based off of assertEqual function

--- head.js
+++ head.js
@@ -6,8 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(array) {
-  return array[0];
+  const head = function(array) {
+    if (array.length === 0) {
+    } else {
+    return array[0];
+  }
 }
 
 assertEqual(head([5,6,7]), 5);
