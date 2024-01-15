5de711a457825fb762f305b126fd3e6724378104
Ethan Pelensky <ethan@arcytekt.com>
2024-01-15 15:55:41 -0700

    refactored version including eqArrays.js with unit testing.

--- eqArrays.js
+++ eqArrays.js
@@ -19,14 +19,6 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+module.exports = eqArrays;
