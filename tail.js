b906f0396f1eecf446252fb8e639511d8ec9bda2
Ethan Pelensky <ethan@arcytekt.com>
2024-01-15 15:46:16 -0700

    updated version of lotide with tail.js refactored to utilize unit testing.

--- tail.js
+++ tail.js
@@ -1,19 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
 };
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words).length, words.length - 1) // tail should have one element less than its input
-assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual')
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words).length, words.length - 1) // tail should have one element less than its input
+assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
