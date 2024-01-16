886add383e373e991de1ec83b1989a8a2f081471
Ethan Pelensky <ethan@arcytekt.com>
2024-01-15 19:55:41 -0700

    updated version integrating chai & mocha tests into tailTest.js & middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,23 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
 
-assertArraysEqual(middle([0, 1, 2]), [1]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([0, 1, 2]), [1]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1]), []);
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("should return [1] for input [0, 1, 2]", () => {
+    assert.deepEqual(middle([0, 1, 2]), [1]);
+  });
+
+  it("should return [2, 3] for input [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("should return an empty array for input [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,35 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual')
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual')
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words).length, words.length - 1) // tail should have one element less than its input
-assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(tail(words).length, words.length - 1) // tail should have one element less than its input
+// assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
+// assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("should not modify the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("should have one element less than its input array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(words).length, words.length - 1);
+  });
+
+  it("should have an empty array when the input array has one element", () => {
+    const result = tail(["test"]);
+    assert.strictEqual(result.length, 0);
+  });
+
+  it("should have an empty array when the input array is empty", () => {
+    const result = tail([]);
+    assert.strictEqual(result.length, 0);
+  });
+});
